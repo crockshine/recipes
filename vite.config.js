0942be5fac593f9923dd45dce6cc362ba5c7ff40
Mihail <mihail_korolev1111@mail.ru>
2024-03-02 17:37:22 +0300

    создали pwa приложение

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const vitePWA = VitePWA({
+  registerType:"autoUpdate",
+  manifest: {
+    name: "recipes",
+    short_name: "recipes",
+    description: "recipes",
+    icons:[
+      {
+        src: "icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"}
+      
+    ],
+    display: "standalone"
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),vitePWA],
   base: "/recipes/"
 })
